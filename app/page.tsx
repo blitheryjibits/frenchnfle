84b7147cc37c8f910b678f0f7f9842b8e933a7ee
robert thornton <robert.thornton92@gmail.com>
2026-02-26 14:20:00 +0200

    Feature - added Nav bar and main content (HomeContent) to the component. The Nav bar contains navigation items and logo that stick to the top of page while home content contains a reference to specific courses

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { EmailForm } from "@/components/forms/EmailForm"; 
 import { Header } from "@/components/Header";
+import { Nav } from "@/components/ui/Nav"
+import { HomeContent } from "@/components/HomeContent";
 
 export default function Home() {
 
-
   return (
-    <div className="min-h-screen p-8 bg-gray-500 flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-gray-500 flex flex-col justify-center">
+      <Nav />
       <Header />
+      <HomeContent />
       <EmailForm />
      
     </div>
