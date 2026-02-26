ba32e7610c1226af13cbc0b5cc8e65396a1c8bb7
robert thornton <robert.thornton92@gmail.com>
2026-02-26 15:16:54 +0200

    Returns an empty page as a placeholder navigation element

--- app/course-de-francais/page.tsx
+++ app/course-de-francais/page.tsx
@@ -1,10 +1,11 @@
 
+import {Nav} from "@/components/ui/Nav"
 
 export const CourseDeFrancais = () => {
 
     return (
-        <>
-        
-        </>
+        <div>
+            <Nav />
+        </div>
     )
 }
