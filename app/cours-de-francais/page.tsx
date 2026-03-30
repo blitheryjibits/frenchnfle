15a5c4b19da8236b32be67ac40e2e1ffa4e72391
robert thornton <robert.thornton92@gmail.com>
2026-03-30 11:28:05 +0300

    added - new section (Preparation aux examens) and made some minor layout changes to make course cards more responsive

--- app/cours-de-francais/page.tsx
+++ app/cours-de-francais/page.tsx
@@ -9,13 +9,16 @@ import {
   Briefcase,
   BookOpen,
   MessagesSquare,
+  CheckCircle,
 } from "lucide-react";
+import Image from "next/image";
 
 export default function CourseDeFrancais() {
   return (
     <main className="w-full h-min-screen flex flex-col justify-center font-playfair">
       <Nav />
       <Hero
+        className="bg-primary-foreground"
         minorText="L'EXCELLENCE ACADÉMIQUE"
         heading={
           <h1 className="font-noto-serif font-bold text-3xl mb-4 md:text-6xl">
@@ -33,8 +36,11 @@ export default function CourseDeFrancais() {
 
       <section className="w-full">
         <div className="w-full px-4 md:px-0 md:w-8/9 mx-auto flex flex-col gap-8">
-          <h2 className="relative text-3xl font-noto-serif before:content-[''] before:absolute before:w-16 before:h-1 before:-bottom-2 before:bg-primary before:left-0">
-            Nos Parcours d&apos;Apprentissage
+          <h2 className="relative text-3xl font-noto-serif ">
+            <span className="relative before:content-[''] before:absolute before:w-full before:h-1 before:bottom-0 before:bg-primary before:left-0">
+              Nos P
+            </span>
+            arcours d&apos;Apprentissage
           </h2>
           <div className="flex flex-col md:flex-row w-full gap-4">
             <MaisonryGridCard
@@ -94,12 +100,78 @@ export default function CourseDeFrancais() {
         </div>
       </section>
 
-      <section className="w-full mt-32">
-        <div className="w-full md:w-8/9 mx-auto flex flex-col gap-8">
-          <h2 className="relative w-fit text-3xl font-noto-serif before:content-[''] before:absolute  before:w-23 before:h-1 before:-bottom-2 before:bg-primary before:right-0">
-            Check Out Our Courses
+      <section className="w-full mt-32 bg-primary-foreground py-12">
+        <div className="w-full h-fit md:px-0 md:w-8/9 mx-auto flex flex-col md:flex-row gap-8 px-6">
+          <div className="w-full md:flex-1 h-100%">
+            <div className="relative max-w-96 aspect-square mx-auto">
+              <Image
+                src="/pen-paper-table.png"
+                alt="pen and paper on a table"
+                fill
+              />
+            </div>
+          </div>
+          <div className="flex-1 flex flex-col gap-4 text-pretty">
+            <h2 className="text-3xl">PRÉPARATION AUX EXAMENS ET TESTS</h2>
+            <p>
+              Maximisez vos chances de réussite avec nos sessions intensives
+              dédiées aux certifications officielles internationales.
+            </p>
+            <ul className="flex flex-col gap-4 text-muted-foreground">
+              <li>
+                <ul>
+                  <li className="font-semibold text-accent flex items-start leading-none">
+                    <span className="inline-block mr-2">
+                      <CheckCircle color="green" />
+                    </span>
+                    DELF-DALF / TEF / TCF
+                  </li>
+                  <li className="ml-8">
+                    Niveaux A1 à C2 couverts avec supports officiels.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul>
+                  <li className="font-semibold text-accent flex items-start leading-none">
+                    <span className="inline-block mr-2">
+                      <CheckCircle color="green" />
+                    </span>
+                    Fidé Suisse / IB DP
+                  </li>
+                  <li className="ml-8">
+                    Spécialisation pour les résidents suisses et élèves du BI.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul>
+                  <li className="font-semibold text-accent flex items-start leading-none">
+                    <span className="inline-block mr-2">
+                      <CheckCircle color="green" />
+                    </span>
+                    Corrections of mock exams
+                  </li>
+                  <li className="ml-8">
+                    Retours détaillés et coaching personnalisé après chaque
+                    examen blanc.
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full mt-32 py-12">
+        <div className="w-full md:w-8/9 mx-auto flex flex-col gap-8 px-6 md:px-0">
+          <h2 className="w-fit text-3xl font-noto-serif">
+            Check Out Our C
+            <span className="relative before:content-[''] before:absolute  before:w-full before:h-1 before:bottom-0 before:bg-primary before:right-0">
+              ourses
+            </span>
           </h2>
-          <div className="flex flex-col mx-auto md:flex-row gap-6">
+          <div className="flex flex-col mx-auto md:flex-row md:flex-wrap md:justify-evenly gap-6">
             <PriceCard
               title="INITIAL"
               duration="30 mins"
@@ -134,9 +206,10 @@ export default function CourseDeFrancais() {
           </div>
         </div>
       </section>
-      <section className="w-full mt-32 bg-linear-60 from-[#ba3ff7] to-secondary">
-        <div className="w-full md:w-8/9 mx-auto flex flex-col gap-4 py-10">
-          <h2 className="text-3xl font-playfair font-semibold">
+
+      <section className="w-full mt-32 bg-linear-60 from-primary-foreground to-primary/10">
+        <div className="w-full md:w-8/9 mx-auto flex flex-col gap-4 py-10 px-6 md:px-0">
+          <h2 className="text-3xl font-playfair font-semibold ">
             Find Out More
           </h2>
           <div className="font-noto-serif flex flex-col md:flex-row ">
